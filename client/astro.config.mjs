2845f60cfc45fc1b22b095c6de8c0035593250a4
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-10-09 20:09:55 -0600

    refactor: :ambulance: Break: fix internationalization
    Break: fix internationalization
    
    Break: fix internationalization

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel"; // O el adaptador que necesites
 
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react()],
-  adapter: vercel(), // Asegúrate de que el adaptador esté instalado
-  vite: {
-    ssr: {
-      noExternal: ["astro-i18next"],
-    },
-  },
 });
