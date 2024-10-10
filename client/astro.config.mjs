a92b1c8814538826de647c053a8e604e8f46c9d5
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-10-09 20:04:46 -0600

    build: :rocket: Lastest
    Lastest
    
    Lastest

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel"; // O el adaptador que necesites
 
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react()],
-  adapter: vercel(), // Asegúrate de que esto esté correctamente configurado
+  adapter: vercel(), // Asegúrate de que el adaptador esté instalado
+  vite: {
+    ssr: {
+      noExternal: ["astro-i18next"],
+    },
+  },
 });
